e77246a33e39e593b9afb0bc2be8398ee9d36961
cench-dev <joystick1980@gmail.com>
2026-04-03 19:54:07 +0400

    fix: moved provide for gitlab_api_url to app-component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { GITLAB_API_URL } from './core/tokens/gitlab-api-url.token';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -9,6 +8,5 @@ export const appConfig: ApplicationConfig = {
         provideBrowserGlobalErrorListeners(),
         provideHttpClient(),
         provideRouter(routes),
-        { provide: GITLAB_API_URL, useValue: 'https://gitlab.com/api/v4' }
     ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GitLabService } from './services/gitlab-request/gitlab-request.service';
+import { GITLAB_API_URL } from './core/tokens/gitlab-api-url.token';
 
 @Component({
     standalone: true,
     selector: 'app-root',
     imports: [RouterOutlet],
-    providers: [GitLabService],
+    providers: [GitLabService,
+        { provide: GITLAB_API_URL, useValue: 'https://gitlab.com/api/v4' }
+    ],
     templateUrl: './app.html',
     styleUrl: './app.scss'
 })
